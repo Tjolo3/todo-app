45b84d2787c03072e32bdf55d00b3ec563930f5c
Tjolo <tjolotjolotjolo3@gmail.com>
2019-01-02 16:57:40 +0100

    Redux tut

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDO from "./pages/todo";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./modules/store";
+
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
+
 
 class App extends Component {
   render() {
     return (
+      <ReduxProvider store={reduxStore}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <h1 className="App-title">ToDo Redux app</h1>
+          </header>
+          <ToDO />
+        </div>
+      </ReduxProvider>
     );
   }
 }
